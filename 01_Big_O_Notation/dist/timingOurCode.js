2b6968b13c9d95c47600b2dc9edebe53a2e5e430
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-04-27 08:19:17 -0500

    added notes on tsc command, I'm having trouble setting the tsc command to auto outDir into dist, I need to work on it later but the command tsc myTSfile.ts --outDir ../dist, works

--- 01_Big_O_Notation/dist/timingOurCode.js
+++ 01_Big_O_Notation/dist/timingOurCode.js
@@ -13,13 +13,58 @@ function addUpTwo(num) {
     return total;
 }
 exports.addUpTwo = addUpTwo;
-addUpTwo(5);
+addUpTwo(6);
 addUpTwo(100);
 // So that's one solution, another solution is: 
 function addUpTo(n) {
     console.log(n * (n + 1) / 2);
     return n * (n + 1) / 2;
+    // and essentially what's happening is 
+    // we add 1 to n rightaway in the parens, 
+    // then mulitply that by n, then divide by two
+    // so for addUpTo(6) it goes: 
+    // 6 * (6 + 1) / 2
+    // 6 * (7) / 2
+    // 42 / 2
+    // 21
+    // basic pemdas 
 }
 exports.addUpTo = addUpTo;
-addUpTo(5);
+addUpTo(6);
 addUpTo(100);
+// The main thing we're trying to do is timing these 
+// is which version is better? 
+/*
+
+Why does it matter which version is better anyway?
+
+Faster?
+
+Less memory intensive?
+
+More readable?
+
+More concise?
+
+All of these are valid concerns and it comes
+down to the situation
+
+Most people agree that the first two, speed and
+memory are the most important
+
+And often come at the expense of readability
+
+First let's look at evaluating speed, how long
+does our code take to execute
+
+How do we measure this?
+
+One way is to use a built-in timing function
+
+Something like this:
+
+*/
+var t1 = performance.now();
+addUpTo(100000000);
+var t2 = performance.now();
+console.log("Time elapsed: ".concat((t2 - t1), " seconds "));
