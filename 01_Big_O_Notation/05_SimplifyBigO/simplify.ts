f7078f435677539d6c5a8fa54544786661a8ee0a
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-05-09 09:05:51 -0500

    Added more notes to simplifying big O

--- 01_Big_O_Notation/05_SimplifyBigO/simplify.ts
+++ 01_Big_O_Notation/05_SimplifyBigO/simplify.ts
@@ -48,3 +48,95 @@ to err on the side of caution and choose
  an algorithm with lower time complexity
 
 */
+
+/*
+Smaller terms don't matter! 
+
+O(n + 10) is just O(n)
+
+O(1000n + 50) is just  O(n)
+
+O(n^2 + 5n + 8) is just O(n^2)
+
+
+
+*/
+
+/* 
+Big O shorthands 
+
+Analyzing complexity with big O can get complicated 
+
+But there are rules of thumb that help
+
+These rules don't always work, but they're a 
+helpful starting point 
+
+1. Arithmetic operations are constant 
+
+2. Variable assignment is constant
+
+3. Accessing elements in an array by index or 
+   object key is constant 
+
+4. In a loop, the complexity is the length of 
+   the loop times the complexity of whatever 
+   happens in the loop
+
+Let's look at an example:
+*/
+
+// function logAtLeastFive(n) {
+//   for (var index = 1; index <= Math.max(5, n); index++) {
+//       console.log(index);
+//   }
+// }
+// // logAtLeastFive(10) // logs 10
+// logAtLeastFive(1) // logs 1 through 5
+// // so it will print all numbers up to n, but 
+// // at a minimum it prints 5
+
+// export default logAtLeastFive
+
+/* 
+So what's the BIG O here? 
+
+How would be categorize the above function
+
+We have a loop that will go to either 5 or n, 
+whichever one is larger 
+
+In our case n doesn't matter, the loop will run 
+however many times is needed, passed in by n
+
+if we pass in 10 million, the loop runs 10 million 
+times, so this is: 
+
+O(n)
+
+Because as n grows, the number of operations 
+grows 
+
+
+*/
+
+/* 
+
+if we looked at another function:
+
+function logAtMostFive(n){
+  for(i = 0; i <= Math.min(5, n); i++){
+    console.log(i);
+  }
+}
+
+This is O(1), it doesn't matter what n is
+
+Remember O(1) is amazing, if we can have runtime 
+of O(1) that's fantastic, it can be rare when 
+dealing with more advanced algo's 
+
+O(n) is ok, it's better than O(n^2)
+
+
+*/
