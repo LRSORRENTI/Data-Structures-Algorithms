9fb91540204c4b3306db8cc8f7a33955cb648147
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-05-11 09:29:23 -0500

    added final question on determining space complexity

--- 01_Big_O_Notation/06_Space_Complexity/03_Quiz/spaceComplexityQuiz.js
+++ 01_Big_O_Notation/06_Space_Complexity/03_Quiz/spaceComplexityQuiz.js
@@ -89,3 +89,28 @@ required to copy every second element
 
 
 */
+
+/*
+Question 4: 
+
+Determine the space complexity for
+ the following function
+
+function subtotals(array) {
+    var subtotalArray = Array(array.length);
+    for (var i = 0; i < array.length; i++) {
+        var subtotal = 0;
+        for (var j = 0; j <= i; j++) {
+            subtotal += array[j];
+        }
+        subtotalArray[i] = subtotal;
+    }
+    return subtotalArray;
+}
+
+A: O(n) because the space complexity will grow as n grows
+proportionately
+
+
+
+*/
