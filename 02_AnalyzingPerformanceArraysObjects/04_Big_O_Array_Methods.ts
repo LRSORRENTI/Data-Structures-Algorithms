dfac4df577159166d3cf211f047c0410d1fa9b7f
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-05-17 08:46:53 -0500

    Added intro to Big O of array methods

---
+++ 02_AnalyzingPerformanceArraysObjects/04_Big_O_Array_Methods.ts
@@ -0,0 +1,37 @@
+/*
+
+======================
+BIG O OF ARRAY METHODS
+======================
+
+Quick note, memorizing this info 
+is not necessary, the goal is to become 
+aware that different methods will 
+have different time and space complexities
+
+Understanding the basics will help you be able 
+to look at an algorithm, and have an informed 
+idea of what it's time complexity is, remember, 
+we count operations as a rule of thumb
+
+Here's a quick overview, YOU DON'T NEED TO MEMORIZE:
+
+- .push() O(1)
+
+- .pop() O(1)
+
+- .shift() O(N)
+
+- .unshift() O(N)
+
+- .concat() O(N)
+
+- .slice() O(N)
+
+- .splice() O(N)
+
+- .sort() O(N * log N)
+
+- .forEach/map/filter/reduce etc.. O(N)
+
+*/
