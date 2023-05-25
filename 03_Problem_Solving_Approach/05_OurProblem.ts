e19e645e1a71683bf6bb517e46c36a3b85facf91
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-05-25 09:22:56 -0500

    Added intro section to chunking, breaking problems down into smaller more manageable problems

---
+++ 03_Problem_Solving_Approach/05_OurProblem.ts
@@ -0,0 +1,52 @@
+// Write a function that takes in a string and 
+// returns a count of each character in the strinf
+
+charCount('aaa'){
+    /*
+        {
+            a: 4
+        }
+    */
+}
+
+charCount("hello"){
+    /*
+    
+    {
+        h: 1, 
+        e: 1, 
+        l: 2, 
+        o: 1
+    }
+    
+    */
+}
+
+charCount("Your PIN number is 1234!"){
+    /*
+     {
+        1: 1, 
+        2: 1,
+        3: 1,
+        4: 1,
+        b: 1, 
+        e: 1,
+        i: 1,
+        m: 1,
+        n: 2,
+        o: 1,
+        p: 1,
+        r: 2,
+        s: 1, 
+        u: 2, 
+        y: 1
+     }  
+
+
+      
+    */
+}
+
+function charCount(str){
+    
+}
