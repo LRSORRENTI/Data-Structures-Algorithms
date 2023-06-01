3829e09415dce20469383fceb1e49f24da343737
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-06-01 09:30:34 -0500

    added revised version of charcount using a function to check to alphanumeric codes, since true / false checks are extremely fast no matter the language, not 100% I'd be able to come up with this solution in an interview, but listening to Colt's refactoring thought process is valuable

--- 03_Problem_Solving_Approach/05_Reflect_&_Refactor/02_refactor.js
+++ 03_Problem_Solving_Approach/05_Reflect_&_Refactor/02_refactor.js
@@ -1,49 +1,51 @@
+"use strict";
 // Let's go back to our last problem 
 // and refactor
-
-export function charCount(string){
-      let obj = {};
-      for(let i = 0; i < string.length; i++){
-         let char = string[i].toLowerCase();
-         // below we add an expression to 
-         // test if a character is alpha-numeric
-         if(/[a-z0-9]/.test(char)){
+exports.__esModule = true;
+exports.charCount = void 0;
+function charCount(string) {
+    var obj = {};
+    for (var i = 0; i < string.length; i++) {
+        var char = string[i].toLowerCase();
+        // below we add an expression to 
+        // test if a character is alpha-numeric
+        if (/[a-z0-9]/.test(char)) {
             // REGEX weeds out anything not a-z, 0-9, 
             // and remember we lowercased it first above 
             // so its /[a-z]/ otherwise we'd need to expoand 
             // the regex to
             // if(/[a-zA-Z0-9]/.test(char))
-            if(obj[char] > 0){
+            if (obj[char] > 0) {
                 // first we check if the value 
                 // is already in the object, if it's 
                 // greater then 0 that means there's 
                 // already at least one
-                obj[char]++
-            } else {
+                obj[char]++;
+            }
+            else {
                 // otherwise, if it's not yet in the 
                 // array, initialize it to one
                 obj[char] = 1;
             }
-         }
-      }
-      return obj;
+        }
+    }
+    return obj;
 }
-
+exports.charCount = charCount;
 // So the above works, if we finished this problem 
 // in an interview setting, the main points our 
 // interviewer may look at are: 
-
 // 1. The for loop, for loops have their use cases 
 //     but in today's landscape a for of loop so 
 //     we don't need to work with 'i' b/c with 
 //     a 'for of' loop on a string, it gives us 
 //     each character immediately
-
-
 // let's refactor!
-const betterCharCount = (str) => {
-    let obj = {};
-    for(let char of str){
+var betterCharCount = function (str) {
+    var obj = {};
+    console.log(obj, "line 46")
+    for (var _i = 0, str_1 = str; _i < str_1.length; _i++) {
+        var char = str_1[_i];
         // now we can eliminate the 
         // str[i].toLowerCase from above 
         // also keep in mind the placement of the 
@@ -60,13 +62,15 @@ const betterCharCount = (str) => {
         // now let's also refactor that if statement
         // block of code
         // if(/[a-z0-9]/.test(char)) {
-             if(isAlphaNumeric(char)) {
-                char = char.toLowerCase()
-                  obj[char] = ++obj[char] || 1
+
+        if (isAlphaNumeric(char)) {
+            char = char.toLowerCase();
+            obj[char] = ++obj[char] || 1;
             // what the above does is it takes 
             // a character from the object, and 
             // if true, add one to it, else initialize 
             // to 1 
+            console.log(obj, "line 73")
         }
         // one thing to keep in mind is that 
         // REGEX check we have above, REGEX is cool
@@ -75,23 +79,21 @@ const betterCharCount = (str) => {
         // that REGEX performance is variable, it's not 
         // a constant, depending on the environment, 
         // and the browser 
-
+        console.log(obj, 'Line 82')
     }
-}
-
+    console.log(obj, 'line 84')
+};
 // We could utilize the built in charCodeAt()
-const isAlphaNumeric = (char) => {
+var isAlphaNumeric = function (char) {
     // initialize code variable that will grab 
     // the character at index 0 since we're 
-    let code = char.charCodeAt(0);
-    if(!(code > 47 && code < 58) && // numeric 0-9
-       !(code > 64 && code < 91) && // upper A-Z
-       !(code > 96 && code < 123)) { // lower a-z 
-            return false
-       }
-       return true
-} 
-
+    var code = char.charCodeAt(0);
+    if (!(code > 47 && code < 58) && // numeric 0-9
+        !(code > 64 && code < 91) && // upper A-Z
+        !(code > 96 && code < 123)) { // lower a-z 
+        return false;
+    }
+    return true;
+};
 // So above in our better charCount we can refactor!
-
-betterCharCount("hello")
+betterCharCount("hello");
