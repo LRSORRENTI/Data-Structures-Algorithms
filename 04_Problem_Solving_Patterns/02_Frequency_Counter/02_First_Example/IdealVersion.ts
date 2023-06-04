811dd04a35e45a758abe28cf654b906789f6a5a8
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-06-04 09:11:48 -0500

    Finished quadratic counter example, executed with ts-node and it's working, now implementing the refactored non O(n^2) version

---
+++ 04_Problem_Solving_Patterns/02_Frequency_Counter/02_First_Example/IdealVersion.ts
@@ -0,0 +1,12 @@
+// Here's a better non-quadratic version 
+// of the frequencyCounter, last version 
+// we used nested loops, which should be avoided 
+// when possible
+
+function freqCount(arr1: Array<number>, arr2: Array<number>): boolean{
+    if(arr1.legnth !== arr2.length){
+        
+    }
+
+}
+
