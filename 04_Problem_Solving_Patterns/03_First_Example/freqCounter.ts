de690fdad0fda67a32b7eccf19c80943a2ad342d
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-06-04 08:44:15 -0500

    Added quadratic version of the frequency counter solution, added comments displaying what's happening every line

--- 04_Problem_Solving_Patterns/03_First_Example/freqCounter.ts
+++ 04_Problem_Solving_Patterns/03_First_Example/freqCounter.ts
@@ -34,17 +34,51 @@ function notIdealSame(arr1: Array<number>, arr2: Array<number>): boolean{
   // a boolean indicating if arr2 contains the squared version of 
   // each element in arr1
      if(arr1.length !== arr2.length){
-      // this conditional
+      // this conditional checks the lengths of 
+      // arr1, and arr2, if they are not equal return false
+      console.log("please pass in two arrays of equal length")
        return false;
      }
      for(let i: number = 0; i < arr1.length; i++){
+      // we have a loop that iterates over every element in 
+      // arr1
         let correctIndex: number = arr2.indexOf(arr1[i] ** 2)
+        // correct index is assigned the index of the 
+        // squared value of the current element in arr1 within 
+        // arr2, the ** will calc the square of the current element
           if(correctIndex === -1 ){
+            // this check will verify if correctIndex is -1, 
+            // which means if the squared value of the current 
+            // looped over element was not found, arr2 doesn't contain 
+            // that squared version of the element in arr1 so return false
+
               return false;
         }
+        // if we make it down here, that means that the squared value 
+        // from arr1 is inside arr2, so we splice that value, ensuring 
+        // we don't count duplicated
         arr2.splice(correctIndex, 1)
      }
+     // and if we make it all the way down here, it means 
+     // every element from arr1 has a squared version of that same 
+     // element present in arr2
      return true
      // Time complexity is O(n ^ 2)
 }
+/* 
+The time complexity of this code is indeed O(n^2),
+ where n is the length of arr1 (and also arr2). 
+ 
+ This is because the indexOf method called within
+  the loop has a time complexity of O(n), and it 
+  is executed for each element in arr1. Additionally, 
+  the splice method within the loop also has a 
+  time complexity of O(n) since it may need to 
+  shift elements after removing an element. 
+  
+  As a result, the overall time complexity is O(n^2).
+
+
+
+*/
 
