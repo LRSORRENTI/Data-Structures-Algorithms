30c4f1df346f8211c561a26bd14e3ea0f3672bff
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-06-05 08:06:03 -0500

    added problem overview, also added 0(1) to the readme, need to finish that later

---
+++ 04_Problem_Solving_Patterns/02_Frequency_Counter/03_Anagram_Challenge/01_firstAttemp.ts
@@ -0,0 +1,30 @@
+/*
+``````````````````
+ANAGRAMS EXERCISE
+
+``````````````````
+
+To test our knowledge of the frequency counter
+we'll try out a problem: 
+
+Given two strings, write a function to 
+determine if the second string is an anagram
+of the first string argument 
+
+An anagram is a word, phrase, or name formed 
+by rearranging the letters of another, such 
+as: 
+
+Cinema is an anagram with Iceman 
+
+'cinema' and 'iceman' true
+
+'cat' and 'tar' // false 
+
+'cat' and 'tac' // True 
+
+
+
+
+
+*/
