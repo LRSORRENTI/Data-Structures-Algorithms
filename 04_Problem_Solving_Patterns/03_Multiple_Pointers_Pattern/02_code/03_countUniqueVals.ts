46cdcc585dba5ed1973afa24f9f6bb7f419d008c
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-06-13 10:22:19 -0500

    added setup for uniqueVals counter

---
+++ 04_Problem_Solving_Patterns/03_Multiple_Pointers_Pattern/02_code/03_countUniqueVals.ts
@@ -0,0 +1,20 @@
+// Implement a function that will accept a sorted 
+// array and counts the unique vals in that array
+
+// There can be negative nums, but it will always 
+// be sorted 
+
+// countUniq([1, 1, 1, 2]) // 2 
+
+// countUniq([2, 2, 3, 4, 5, 5, 6]) // 5
+
+const countUniq = (arr) => {
+    // first declare variable i set to 0
+     let i = 0;
+     // then start a loop, 
+     for(let j = 1; j < arr.length; j++){
+        // j is going to be our pointer 
+        // it will go through the array
+     }
+}
+
