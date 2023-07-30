9c7611a5b8772e60c72d45c69a9cc23545e51478
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-07-30 08:07:07 -0500

    added more comments inside of the binary search function, adding more explanations as to what the purpose of the pointers are

--- 07_Binary_Search/02-Binary-Pseudo.ts
+++ 07_Binary_Search/02-Binary-Pseudo.ts
@@ -1,12 +1,14 @@
-// BINARY SEARCH PSEUDCODE
+// BINARY SEARCH PSEUDO-CODE
 
 // Function that accepts a sorted array, 
 // it must be sorted, this function also 
 // accepts a value
 
 // Create a left pointer, at the start of the 
-// array, and a right pointer at the end of 
-// the array
+// array, always at index[0], 
+//and a right pointer at the end of 
+// the array, and a middle pointer, about roughly 
+// halfway between the starting and ending pointer 
 
 // While loop, while the left pointer comes 
 // before the right pointer: 
@@ -29,51 +31,79 @@
 
 function binarySearch(sortedArr: number[], element: number){
     let start = 0;
-    // start is our left pointer
+
+    // start is our left pointer, index of 0
+
     let end = sortedArr.length -1;
-    // end is our right pointer
+    // end is our right pointer, which is the length - 1,
+    // b/c without the - 1, we'd be out of bounds, outside 
+    // the end, arr.length - 1 is the actual last 
+    // array value  
+
     let middlePointer = Math.floor((start + end) / 2);
+
+    // the middle pointer will be the starting index, 
+    // pus the length of the array, which can be 
+    // any length, divided by 2, then we floor that 
+    // number so we're not looking a floating point number 
+    
+    // There's never going to be an arrray index[3.5] 
+    // indexes of an array are always whole numbers
+
     // middlepointer will grab the middle of
     // the array by adding 0 + the length 
     // of the array, and dividing by twi, it will 
     // also round  down if we get a decimal number
         // console.log(start, middlePointer, end)
+
         while(sortedArr[middlePointer] !== element){
             console.log(start, middlePointer, end)
+
             // so if we're in here we can assumne 
             // the middle pointer is not yet at the 
             // desired element, we haven't found 
             // what we're searching for yet
+
+        // binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 15)
+            //           S         M              E
+
 
             // so we want 15, 15 is greater than the 
             // middle pointer, so we now need to move 
             // the start pointer to 13, because we now 
             // know for sure it's not 2, 5, 6, or 9
             // so we can skip checking those 
+
             if(element < sortedArr[middlePointer]){
                 // so if our element is less than where 
                 // the pointer is at, we want to move 
                 // the end pointer where the middle 
                 // pointer just was minus 1
+
                 end = middlePointer - 1;
 
             } else {
+
                 // Else means that what we're looking for 
                 // is greater than the middle pointer
                 
                 start = middlePointer + 1;
 
             }
-                // So at this point the End is still the same 
+               
+            // So at this point the End is still the same 
                 // but if we're looking for 15 in the array below, 
                 // that start pointer moves up one to 13, but 
                 //  we need to recalculate / average the middle:
+               
                 middlePointer = Math.floor((start + end) / 2);
+                
                 // this will give us Math.floor(4 , 7)
                 // since the start pointer is now at 13 aka index 4,
                 
                 console.log(start, middlePointer, end)
         }
+       
         // we'll loop while the middle pointer is not 
         // equal to the element we want
 }
