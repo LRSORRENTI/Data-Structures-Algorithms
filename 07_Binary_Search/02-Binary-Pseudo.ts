2003f4a038fbe06c03ac7f0a3afee965f318a230
Luke Sorrenti <lrsorrenti@hotmail.com>
2023-07-30 05:04:26 -0500

    Added more notes on Binary Search

--- 07_Binary_Search/02-Binary-Pseudo.ts
+++ 07_Binary_Search/02-Binary-Pseudo.ts
@@ -38,6 +38,24 @@ function binarySearch(sortedArr: number[], element: number){
     // of the array, and dividing by twi, it will 
     // also round  down if we get a decimal number
         console.log(start, middlePointer, end)
+        while(sortedArr[middlePointer] !== element){
+            // so if we're in here we can assumne 
+            // the middle pointer is not yet at the 
+            // desired eleement
+
+            // so we want 15, 15 is greater than the 
+            // middle pointer, so we now need to move 
+            // the start pointer to 13, because we now 
+            // know for sure it's not 2, 5, 6, or 9
+            // so we can skip checking those 
+            if(element < sortedArr[middlePointer]){
+                // so if our element 
+            }
+
+
+        }
+        // we'll loop while the middle pointer is not 
+        // equal to the element we want
 }
 
 binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 15)
